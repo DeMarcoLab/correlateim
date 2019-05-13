316b9929feb022de6c6e9b788a26661416258c6e
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-05-13 17:49:39 +1000

    Utility files for services

---
+++ README.md
@@ -0,0 +1,50 @@
+#
+
+## Quickstart
+install from release binary
+see the video/gif
+
+## Installation
+
+```
+pip install git+https://github.com/DeMarcoLab/correlateim.git
+```
+
+## Running the program
+### Demo data
+
+
+## For developers
+### Development installation
+```
+git clone https://github.com/DeMarcoLab/correlateim.git
+cd correlateim
+```
+
+```
+pip install -r requirements.txt
+pip install -r requirements-dev.txt
+```
+
+```
+pip install -e .
+```
+
+### Running the tests
+
+```
+py.test
+```
+
+#### Generating new baseline images for pytest matplotlib plugin
+
+```
+py.test --mpl-generate-path=tests\baseline
+```
+
+### Packaging
+Pyinstaller is used to create binaries: https://www.pyinstaller.org/
+
+```
+pyinstaller correlateim/main.py
+```
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+from correlateim._version import __version__
+
+descr = """2D image correlation using matched points."""
+
+DISTNAME = 'correlateim'
+DESCRIPTION = '2D image correlation using matched points.'
+LONG_DESCRIPTION = descr
+MAINTAINER = 'Genevieve Buckley'
+URL = 'https://github.com/DeMarcoLab/correlateim'
+DOWNLOAD_URL = 'https://github.com/DeMarcoLab/correlateim'
+VERSION = __version__
+PYTHON_VERSION = (3, 5)
+INST_DEPENDENCIES = [
+    'cpselect',
+    'matplotlib',
+    'numpy',
+    'Pillow',
+    'pyqt5',
+    'scikit-image'
+]
+
+if __name__ == '__main__':
+    setup(name=DISTNAME,
+          version=__version__,
+          url=URL,
+          description=DESCRIPTION,
+          long_description=LONG_DESCRIPTION,
+          author=MAINTAINER,
+          packages=find_packages(),
+          install_requires=INST_DEPENDENCIES
+          )
