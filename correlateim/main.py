cda11033ebba3fad3cc20261a94e04a1b2feb280
GenevieveBuckley <30920819+GenevieveBuckley@users.noreply.github.com>
2019-05-15 00:57:40 +1000

    Integrate cpselect module

--- correlateim/main.py
+++ correlateim/main.py
@@ -1,12 +1,14 @@
 import os
 
 import click
-from cpselect.cpselect import cpselect
 import matplotlib.pyplot as plt
+import skimage.color
+import skimage.transform
 
 from correlateim import io
 from correlateim import imageproc
 from correlateim import transform
+from correlateim.cpselect import cpselect
 
 
 @click.command()
@@ -14,22 +16,22 @@ from correlateim import transform
 @click.argument('input_filename_2')
 @click.argument('output_filename')
 def main(input_filename_1, input_filename_2, output_filename):
-    image_1 = io.imread(input_filename_1)
-    image_2 = io.imread(input_filename_2)
-    temp_filename = os.path.join(os.path.split(output_filename)[0],
-                                               'temp_image1.tif')
-    image_1 = io.resize_and_save(temp_filename, image_1, image_2.shape)
     # User select matched control points
-    matched_points_dict = cpselect(temp_filename, input_filename_2)
+    matched_points_dict = cpselect(input_filename_1, input_filename_2)
     print(matched_points_dict)
     # Calculate and apply affine transformation
     src, dst = transform.point_coords(matched_points_dict)
     transformation = transform.calculate_transform(src, dst)
+    # Align images
+    image_1 = skimage.color.gray2rgb(plt.imread(input_filename_1))
+    image_2 = skimage.color.gray2rgb(plt.imread(input_filename_2))
+    image_1 = skimage.transform.resize(image_1, image_2.shape,
+                                       anti_aliasing=True)
     image_1_aligned = transform.apply_transform(image_1, transformation)
     result = imageproc.overlay_images(image_1_aligned, image_2)
-    # Finish and tidy up
-    os.remove(temp_filename)
-    io.imsave(output_filename, result)
+    # Finish and tidy .
+    output_filename = os.path.join(os.path.splitext(output_filename)[0], '.png')
+    plt.imsave(output_filename, result)
     plt.imshow(result)
     plt.show()
     return result
